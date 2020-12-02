73b9233e5022de9b5f68b21085e71bd8441d71e2
tkellehe <mitchell.kelleher@gmail.com>
2020-12-02 17:02:31 -0500

    Update iterator.h

--- bst/iterator.h
+++ bst/iterator.h
@@ -14,39 +14,63 @@ extern "C" {
 /* Provide without namespace */
 /// \{
 #ifdef BST_NO_NAMESPACE
+#define iter_ppack_t bst_iter_ppack_t
+#define iter_ppack_nxt bst_iter_ppack_nxt
+#define iter_ppack_eq bst_iter_ppack_eq
+#define iter_ppack_val bst_iter_ppack_val
+#define iter_ppack_set bst_iter_ppack_set
+
+#define iter_ppack bst_iter_ppack
+#define iter_t bst_iter_t
+#define iter_nxt bst_iter_nxt
+#define iter_eq bst_iter_eq
+#define iter_val bst_iter_val
+#define iter_set bst_iter_set
+
+#define iter_defaults bst_iter_defaults
+#define iter_no_t bst_iter_no_t
+#define iter_nonxt bst_iter_nonxt
+#define iter_noeq bst_iter_noeq
+#define iter_noval bst_iter_noval
+#define iter_noset bst_iter_noset
 #endif
 /// \}
 
 
 /* Pack arguments provided into a single ppack maintaining the standard order where defaults are no-ops */
 /// \{
+#define bst_iter_ppack_t(t) bst_ppack(t, bst_iter_nonxt, bst_iter_noeq, bst_iter_noval, bst_iter_noset)
+#define bst_iter_ppack_nxt(nxt) bst_ppack(bst_iter_no_t, nxt, bst_iter_noeq, bst_iter_noval, bst_iter_noset)
+#define bst_iter_ppack_eq(eq) bst_ppack(bst_iter_no_t, bst_iter_nonxt, eq, bst_iter_noval, bst_iter_noset)
+#define bst_iter_ppack_val(val) bst_ppack(bst_iter_no_t, bst_iter_nonxt, bst_iter_noeq, val, bst_iter_noset)
+#define bst_iter_ppack_set(set) bst_ppack(bst_iter_no_t, bst_iter_nonxt, bst_iter_noeq, bst_iter_noval, set)
+
 /* Packs assuming everything is provided in the std order */
-#define bst_iter_ppack(hmmm, ...) bst_ppack(hmmm)
+#define bst_iter_ppack(...) \
+    bst_ppack_defaults(\
+        bst_ppack(__VA_ARGS__),\
+        bst_iter_defaults\
+    )
+
+#define bst_iter_t(pkd) bst_ppack_argI(pkd, 0)
+#define bst_iter_nxt(pkd) bst_ppack_argI(pkd, 1)
+#define bst_iter_eq(pkd) bst_ppack_argI(pkd, 2)
+#define bst_iter_val(pkd) bst_ppack_argI(pkd, 3)
+#define bst_iter_set(pkd) bst_ppack_argI(pkd, 4)
 /// \}
 
 
-/* Default do-nothing memory management functions */
+/* Packs the iterator defaults */
+#define bst_iter_defaults bst_ppack(bst_iter_no_t, bst_iter_nonxt, bst_iter_noeq, bst_iter_noval, bst_iter_noset)
+
+
+/* Default do-nothing iterator functions */
 /// \{
-// These may need the ppack of the template type.
-// #define bst_iter_no_t(...) int
-// #define bst_iter_nobegin(...) (bst_null)
-// #define bst_iter_noend(...) (bst_null)
-
-// #define bst_iter_nonxt(iter) (++iter)
-// #define bst_iter_noeq(left, right) (left == right)
-// #define bst_iter_noneq(left, right) (left != right)
-// #define bst_iter_noval(iter) (())
-// #define bst_iter_noset(iter, val) (val)
-
-// #define bst_vect_rbegin(vect) ((vect)+bst_vect_cnt(vect))
-// #define bst_vect_rend(vect) ((vect)+1)
-
-// #define bst_vect_riter_t(bst_vect_t) bst_vect_t
-// #define bst_vect_riter_nxt(iter) (--iter)
-// #define bst_vect_riter_eq(left, right) (left == right)
-// #define bst_vect_riter_neq(left, right) (left != right)
-// #define bst_vect_riter_val(iter) *((iter)-1)
-// #define bst_vect_riter_set(iter, val) (*((iter)-1) = val)
+#define bst_iter_no_t(...) int
+#define bst_iter_nonxt(...) ((void)0)
+#define bst_iter_noeq(...) 0
+#define bst_iter_noval(...) ((void)0)
+#define bst_iter_noset(...) ((void)0)
 /// \}
 
 
