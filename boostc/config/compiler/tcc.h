d37af0262a976a893a6d4141cf0566839cfafed3
tkellehe <mitchell.kelleher@gmail.com>
2020-12-18 07:25:38 -0500

    tcc
    tcc supports everything just like gcc (except tcc does not behave the same with empty token pasting as gcc does...)

---
+++ boostc/config/compiler/tcc.h
@@ -0,0 +1,28 @@
+//  Copyright (C) 2020-? T. Mitchell Kelleher
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+#ifndef BSTC__CONFIG__COMPILER__TCC_H
+#define BSTC__CONFIG__COMPILER__TCC_H
+
+
+/* Provide a way to detect the version of TCC. */
+/// \{
+#define BSTC_TCC_VER __TINYC__
+/// \}
+
+
+/* Not 100% if always has this... */
+/// \{
+#define BSTC_HAS_LONG_LONG
+/// \}
+
+
+/* Indicate that null concat with __VA_ARGS__ works. Turns out does not work the exact same as gcc. */
+/// \{
+// #define BSTC_HAS_VA_ARGS_PASTE
+/// \}
+
+
+#endif // BSTC__CONFIG__COMPILER__TCC_H
