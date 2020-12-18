5874410235473ceb9ddbea393ce4d56340af1cf0
tkellehe <mitchell.kelleher@gmail.com>
2020-12-18 15:40:05 -0500

    iterator passed

--- boostc/iterator.h
+++ boostc/iterator.h
@@ -1,53 +1,215 @@
-#ifndef BSTC__ITERATOR_H
-#define BSTC__ITERATOR_H
-
-
+//  Copyright (C) 2020-? T. Mitchell Kelleher
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
 #include <boostc/config.h>
 #include <boostc/ctuple.h>
 
 
-/* Pack arguments provided into a single ctuple maintaining the standard order where defaults are no-ops */
+/** Creates a ctuple with the defaults set except for the iterator type provided. */
 /// \{
-#define bstc_iter_ctuple_t(t) bstc_iter_set_t(bstc_iter_defaults, t)
-#define bstc_iter_ctuple_nxt(nxt) bstc_iter_set_nxt(bstc_iter_defaults, nxt)
-#define bstc_iter_ctuple_eq(eq) bstc_iter_set_eq(bstc_iter_defaults, eq)
-#define bstc_iter_ctuple_val(val) bstc_iter_set_val(bstc_iter_defaults, val)
-#define bstc_iter_ctuple_set(set) bstc_iter_set_set(bstc_iter_defaults, set)
-#define bstc_iter_ctuple_swap(swap) bstc_iter_set_swap(bstc_iter_defaults, swap)
-
-#define bstc_iter_t(tpl) bstc_ctuple_getI(tpl, 0)
-#define bstc_iter_nxt(tpl) bstc_ctuple_getI(tpl, 1)
-#define bstc_iter_eq(tpl) bstc_ctuple_getI(tpl, 2)
-#define bstc_iter_val(tpl) bstc_ctuple_getI(tpl, 3)
-#define bstc_iter_set(tpl) bstc_ctuple_getI(tpl, 4)
-#define bstc_iter_swap(tpl) bstc_ctuple_getI(tpl, 5)
-
-#define bstc_iter_set_t(tpl, t) bstc_ctuple_setI(tpl, 0, t)
-#define bstc_iter_set_nxt(tpl, nxt) bstc_ctuple_setI(tpl, 1, nxt)
-#define bstc_iter_set_eq(tpl, eq) bstc_ctuple_setI(tpl, 2, eq)
-#define bstc_iter_set_val(tpl, val) bstc_ctuple_setI(tpl, 3, val)
-#define bstc_iter_set_set(tpl, set) bstc_ctuple_setI(tpl, 4, set)
-#define bstc_iter_set_swap(tpl, swap) bstc_ctuple_setI(tpl, 5, swap)
+#ifndef bstc_iter_pack_t
+# define bstc_iter_pack_t(t) bstc_iter_set_t(bstc_iter_defaults, t)
+#endif
 /// \}
 
 
-/* Packs the iterator defaults */
-#define bstc_iter_defaults bstc_ctuple(bstc_iter_no_t, bstc_iter_nonxt, bstc_iter_noeq, bstc_iter_noval, bstc_iter_noset, bstc_iter_noswap)
-
-
-/* Default do-nothing iterator functions */
+/** Creates a ctuple with the defaults set except for the nxt function provided. */
 /// \{
-#define bstc_iter_no_t(...) int
-#define bstc_iter_nonxt(...) ((void)0)
-#define bstc_iter_noeq(...) 0
-#define bstc_iter_noval(...) ((void)0)
-#define bstc_iter_noset(...) ((void)0)
-#define bstc_iter_noswap(...) ((void)0)
+#ifndef bstc_iter_pack_nxt
+# define bstc_iter_pack_nxt(nxt) bstc_iter_set_nxt(bstc_iter_defaults, nxt)
+#endif
 /// \}
 
 
-/* Detect if provided a ctuple that could be an iterator */
-#define bstc_iter_isa(tpl, _t, _f) bstc_ctuple_isa(tpl, bstc_ctuple_hasN(tpl, 6, _t, _f), _f)
+/** Creates a ctuple with the defaults set except for the eq function provided. */
+/// \{
+#ifndef bstc_iter_pack_eq
+# define bstc_iter_pack_eq(eq) bstc_iter_set_eq(bstc_iter_defaults, eq)
+#endif
+/// \}
 
 
-#endif // BSTC__ITERATOR_H
+/** Creates a ctuple with the defaults set except for the val function provided. */
+/// \{
+#ifndef bstc_iter_pack_val
+# define bstc_iter_pack_val(val) bstc_iter_set_val(bstc_iter_defaults, val)
+#endif
+/// \}
+
+
+/** Creates a ctuple with the defaults set except for the set function provided. */
+/// \{
+#ifndef bstc_iter_pack_set
+# define bstc_iter_pack_set(set) bstc_iter_set_set(bstc_iter_defaults, set)
+#endif
+/// \}
+
+
+/** Creates a ctuple with the defaults set except for the swap function provided. */
+/// \{
+#ifndef bstc_iter_pack_swap
+# define bstc_iter_pack_swap(swap) bstc_iter_set_swap(bstc_iter_defaults, swap)
+#endif
+/// \}
+
+
+/** Get the iterator type that has been packed into the ctuple. */
+/// \{
+#ifndef bstc_iter_t
+# define bstc_iter_t(tpl) bstc_ctuple_getI(tpl, 0)
+#endif
+/// \}
+
+
+/** Get the nxt function that has been packed into the ctuple. */
+/// \{
+#ifndef bstc_iter_nxt
+# define bstc_iter_nxt(tpl) bstc_ctuple_getI(tpl, 1)
+#endif
+/// \}
+
+
+/** Get the eq function that has been packed into the ctuple. */
+/// \{
+#ifndef bstc_iter_eq
+# define bstc_iter_eq(tpl) bstc_ctuple_getI(tpl, 2)
+#endif
+/// \}
+
+
+/** Get the val function that has been packed into the ctuple. */
+/// \{
+#ifndef bstc_iter_val
+# define bstc_iter_val(tpl) bstc_ctuple_getI(tpl, 3)
+#endif
+/// \}
+
+
+/** Get the set function that has been packed into the ctuple. */
+/// \{
+#ifndef bstc_iter_set
+# define bstc_iter_set(tpl) bstc_ctuple_getI(tpl, 4)
+#endif
+/// \}
+
+
+/** Get the swap function that has been packed into the ctuple. */
+/// \{
+#ifndef bstc_iter_swap
+# define bstc_iter_swap(tpl) bstc_ctuple_getI(tpl, 5)
+#endif
+/// \}
+
+
+/** Create a copy of the ctuple as an iterator with the iterator type changed to what is provided. */
+/// \{
+#ifndef bstc_iter_set_t
+# define bstc_iter_set_t(tpl, t) bstc_ctuple_setI(tpl, 0, t)
+#endif
+/// \}
+
+
+/** Create a copy of the ctuple as an iterator with the nxt function changed to what is provided. */
+/// \{
+#ifndef bstc_iter_set_nxt
+# define bstc_iter_set_nxt(tpl, nxt) bstc_ctuple_setI(tpl, 1, nxt)
+#endif
+/// \}
+
+
+/** Create a copy of the ctuple as an iterator with the eq function changed to what is provided. */
+/// \{
+#ifndef bstc_iter_set_eq
+# define bstc_iter_set_eq(tpl, eq) bstc_ctuple_setI(tpl, 2, eq)
+#endif
+/// \}
+
+
+/** Create a copy of the ctuple as an iterator with the val function changed to what is provided. */
+/// \{
+#ifndef bstc_iter_set_val
+# define bstc_iter_set_val(tpl, val) bstc_ctuple_setI(tpl, 3, val)
+#endif
+/// \}
+
+
+/** Create a copy of the ctuple as an iterator with the set function changed to what is provided. */
+/// \{
+#ifndef bstc_iter_set_set
+# define bstc_iter_set_set(tpl, set) bstc_ctuple_setI(tpl, 4, set)
+#endif
+/// \}
+
+
+/** Create a copy of the ctuple as an iterator with the swap function changed to what is provided. */
+/// \{
+#ifndef bstc_iter_set_swap
+# define bstc_iter_set_swap(tpl, swap) bstc_ctuple_setI(tpl, 5, swap)
+#endif
+/// \}
+
+
+/** Default iterator ctuple that uses the no-op iterator functions. */
+/// \{
+#ifndef bstc_iter_defaults
+# define bstc_iter_defaults bstc_ctuple(bstc_iter_no_t, bstc_iter_nonxt, bstc_iter_noeq, bstc_iter_noval, bstc_iter_noset, bstc_iter_noswap)
+#endif
+/// \}
+
+
+/** Default iterator type that is just int. */
+/// \{
+#ifndef bstc_iter_no_t
+# define bstc_iter_no_t int
+#endif
+/// \}
+
+
+/** Default nxt function that does nothing. */
+/// \{
+#ifndef bstc_iter_nonxt
+# define bstc_iter_nonxt(iter) ((void)0)
+#endif
+/// \}
+
+
+/** Default eq function that does nothing. */
+/// \{
+#ifndef bstc_iter_noeq
+# define bstc_iter_noeq(left, right) 0
+#endif
+/// \}
+
+
+/** Default val function that does nothing. */
+/// \{
+#ifndef bstc_iter_noval
+# define bstc_iter_noval(iter) ((void)0)
+#endif
+/// \}
+
+
+/** Default set function that does nothing. */
+/// \{
+#ifndef bstc_iter_noset
+# define bstc_iter_noset(iter, val) ((void)0)
+#endif
+/// \}
+
+
+/** Default swap function that does nothing. */
+/// \{
+#ifndef bstc_iter_noswap
+# define bstc_iter_noswap(left, right) ((void)0)
+#endif
+/// \}
+
+
+/** Detects if the ctuple provided could be a valid iterator ctuple. */
+/// \{
+#ifndef bstc_iter_isa
+# define bstc_iter_isa(tpl, _t, _f) BSTC_EXPAND(bstc_ctuple_isa(tpl, bstc_ctuple_hasN(tpl, 6, _t, _f), _f))
+#endif
+/// \}
---
+++ test/boostc/iterator/main.c
@@ -0,0 +1,45 @@
+#include <boostc/iterator.h>
+#include <boostc/stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+    int num_pass = 0;
+    int num_fail = 0;
+
+    #define iter1 bstc_iter_pack_t(A)
+    #define iter2 bstc_iter_pack_nxt(B)
+    #define iter3 bstc_iter_pack_eq(C)
+    #define iter4 bstc_iter_pack_val(D)
+    #define iter5 bstc_iter_pack_set(E)
+    #define iter6 bstc_iter_pack_swap(F)
+    #define iterN bstc_ctuple(1, 2, 3, 4)
+
+    //--------------------------------------------------------------------------------------------------------
+    printf("bstc_iter_isa(%s)\n", bstc_ctuple_tostring(iter1));
+    bstc_iter_isa(iter1, (++num_pass, printf("    passed\n")), (++num_fail, printf("    failed\n")));
+    printf("bstc_iter_isa(%s)\n", bstc_ctuple_tostring(iter2));
+    bstc_iter_isa(iter2, (++num_pass, printf("    passed\n")), (++num_fail, printf("    failed\n")));
+    printf("bstc_iter_isa(%s)\n", bstc_ctuple_tostring(iter3));
+    bstc_iter_isa(iter3, (++num_pass, printf("    passed\n")), (++num_fail, printf("    failed\n")));
+    printf("bstc_iter_isa(%s)\n", bstc_ctuple_tostring(iter4));
+    bstc_iter_isa(iter4, (++num_pass, printf("    passed\n")), (++num_fail, printf("    failed\n")));
+    printf("bstc_iter_isa(%s)\n", bstc_ctuple_tostring(iter5));
+    bstc_iter_isa(iter5, (++num_pass, printf("    passed\n")), (++num_fail, printf("    failed\n")));
+    printf("bstc_iter_isa(%s)\n", bstc_ctuple_tostring(iter6));
+    bstc_iter_isa(iter6, (++num_pass, printf("    passed\n")), (++num_fail, printf("    failed\n")));
+    printf("bstc_iter_isa(%s)\n", bstc_ctuple_tostring(iterN));
+    bstc_iter_isa(iterN, (++num_fail, printf("    failed\n")), (++num_pass, printf("    passed\n")));
+    printf("bstc_iter_isa(%s)\n", bstc_ctuple_tostring(bstc_iter_defaults));
+    bstc_iter_isa(bstc_iter_defaults, (++num_pass, printf("    passed\n")), (++num_fail, printf("    failed\n")));
+
+    //--------------------------------------------------------------------------------------------------------
+    printf("-----------------------------\n");
+    printf("     pass     |     fail     \n");
+    printf("     %04i     |     %04i     \n", num_pass, num_fail);
+    printf("-----------------------------\n");
+
+    return num_fail ? bstc_exit_failure : bstc_exit_success;
+}
