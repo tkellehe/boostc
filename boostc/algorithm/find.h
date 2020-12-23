1001a57e471391c200d655583f44ae93192d4032
tkellehe <mitchell.kelleher@gmail.com>
2020-12-22 21:33:16 -0500

    find & iterators

--- boostc/algorithm/find.h
+++ boostc/algorithm/find.h
@@ -3,23 +3,17 @@
 //  Distributed under the Boost Software License, Version 1.0. (See
 //  accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt)
-#ifndef BOOSTC__ALGORITHM__FIND_H
-#define BOOSTC__ALGORITHM__FIND_H
-
-
 #include <boostc/config.h>
 #include <boostc/iterator.h>
 
 
-/* Uses the BSTC iterator interface to interact with unordered content */
+/** Uses the BSTC iterator interface to search for the provided value.
+ * \param iters The iterator ctuple to extract the iterator interface from.
+ * \param iter A reference to the iterator to apply the functions to and will be updated with the location.
+ * \param end A reference to the iterator representing the end of the linear search.
+ */
 /// \{
-#define bstc_alg_find(iters, iter, end, val) bstc_alg_findpred(iters, iter, end, (bstc_iter_val(iters)(iter) == val))
-#define bstc_alg_findpred(iters, iter, end, pred) \
-    {\
-        for(; !(bstc_iter_eq(iters)(iter, end)); bstc_iter_nxt(iters)(iter))\
-            if((pred)) break;\
-    }
-/// \}
-
-
+#ifndef bstc_alg_find
+# define bstc_alg_find(iters, iter, end, val) bstc_iter_foreach(iters, iter, end, {if((bstc_iter_val(iters)(iter) == (val)))break;})
 #endif
+/// \}
--- boostc/iterator.h
+++ boostc/iterator.h
@@ -213,3 +213,20 @@
 # define bstc_iter_isa(tpl, _t, _f) BSTC_EXPAND(bstc_ctuple_isa(tpl, bstc_ctuple_hasN(tpl, 6, _t, _f), _f))
 #endif
 /// \}
+
+
+/** Uses the BSTC iterator interface to apply a foreach based off of a perdicate.
+ * \param iters The iterator ctuple to extract the iterator interface from.
+ * \param iter A reference to the iterator to apply the functions to and will be updated with the location.
+ * \param end A reference to the iterator representing the end of the linear search.
+ * \param expr The expression to run each iteration.
+ */
+/// \{
+#ifndef bstc_iter_foreach
+# define bstc_iter_foreach(iters, iter, end, expr) \
+    {\
+        for(; !(bstc_iter_eq(iters)(iter, end)); bstc_iter_nxt(iters)(iter))\
+            {expr}\
+    }
+#endif
+/// \}
