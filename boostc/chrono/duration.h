eaebcc3c80f7d70bcd84248a1b7bac6898a5a017
tkellehe <mitchell.kelleher@gmail.com>
2021-08-19 08:28:31 -0400

    duration cast
    Added duration cast which means do not need to create add functions since can just cast to the same type then do simple adds/subs/etc...

--- boostc/chrono/duration.h
+++ boostc/chrono/duration.h
@@ -65,7 +65,7 @@
 /// \{
 #ifndef bstc_chrono_dur_per
 # ifdef BSTC_HAS_VARIADIC_MACROS
-#  define bstc_chrono_dur_per(tpl) BSTC_EXPAND(BSTC_GET_ARG0 tpl)
+#  define bstc_chrono_dur_per(tpl) BSTC_EXPAND(BSTC_GET_ARG1 tpl)
 # else
 #  define bstc_dtl_chrono_dur_per(rep, per) per
 #  define bstc_chrono_dur_per(tpl) BSTC_EXPAND(bstc_dtl_chrono_dur_per tpl)
@@ -82,6 +82,26 @@
 /// \}
 
 
+/** Converts a duration type into a different duration type.
+ * \param to_dur_tpl The duration type to convert to.
+ * \param dur_tpl The current duration type provided.
+ * \param dur The duration object to be converted.
+ * \return Returns the duration ticks converted to the new duration type.
+ */
+/// \{
+#ifndef bstc_chrono_dur_cast
+# define bstc_chrono_dur_cast(to_dur_tpl, dur_tpl, dur) \
+    ((bstc_chrono_dur_rep(to_dur_tpl))(\
+        (\
+            (dur) * \
+            ((bstc_chrono_dur_rep(to_dur_tpl))((bstc_ratio_num(bstc_chrono_dur_per(dur_tpl))) * (bstc_ratio_den(bstc_chrono_dur_per(to_dur_tpl)))))\
+        ) / \
+        ((bstc_ratio_den(bstc_chrono_dur_per(dur_tpl))) * (bstc_ratio_num(bstc_chrono_dur_per(to_dur_tpl))))\
+    ))
+#endif
+/// \}
+
+
 /** Duration helper types.
  * Note: each of the predefined duration types up to hours covers a range of at least ±292 years. 
  * Each of the predefined duration types days, weeks, months and years covers a range of at least ±40000 years.
@@ -90,71 +110,71 @@
  */
 /// \{
 #ifndef bstc_chrono_ns
-# if bstc_intmax_max >= bstc_uint32_max && defined(bstc_nano)
-#  define bstc_chrono_ns BSTC_CTUPLE2(bstc_uint_least64_t, bstc_nano)
+# if bstc_intmax_max >= bstc_int32_max && defined(bstc_nano)
+#  define bstc_chrono_ns BSTC_CTUPLE2(bstc_int_least64_t, bstc_nano)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_us
-# if bstc_intmax_max >= bstc_uint32_max && defined(bstc_micro)
-#  define bstc_chrono_us BSTC_CTUPLE2(bstc_uint_least64_t, bstc_micro)
+# if bstc_intmax_max >= bstc_int32_max && defined(bstc_micro)
+#  define bstc_chrono_us BSTC_CTUPLE2(bstc_int_least64_t, bstc_micro)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_ms
-# if bstc_intmax_max >= bstc_uint16_max && defined(bstc_milli)
-#  define bstc_chrono_ms BSTC_CTUPLE2(bstc_uint_least64_t, bstc_milli)
+# if bstc_intmax_max >= bstc_int16_max && defined(bstc_milli)
+#  define bstc_chrono_ms BSTC_CTUPLE2(bstc_int_least64_t, bstc_milli)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_sec
-# if defined(bstc_uint_least64_t)
-#  define bstc_chrono_sec BSTC_CTUPLE2(bstc_uint_least64_t, BSTC_CTUPLE2(bstc_ratio_c(1), bstc_ratio_c(1)))
+# if defined(bstc_int_least64_t)
+#  define bstc_chrono_sec BSTC_CTUPLE2(bstc_int_least64_t, BSTC_CTUPLE2(bstc_ratio_c(1), bstc_ratio_c(1)))
 # endif
 #endif
 
 
 #ifndef bstc_chrono_min
-# if defined(bstc_uint_least32_t)
-#  define bstc_chrono_min BSTC_CTUPLE2(bstc_uint_least32_t, BSTC_CTUPLE2(bstc_ratio_c(60), bstc_ratio_c(1)))
+# if defined(bstc_int_least32_t)
+#  define bstc_chrono_min BSTC_CTUPLE2(bstc_int_least32_t, BSTC_CTUPLE2(bstc_ratio_c(60), bstc_ratio_c(1)))
 # endif
 #endif
 
 
 #ifndef bstc_chrono_hrs
-# if defined(bstc_uint_least32_t)
-#  define bstc_chrono_hrs BSTC_CTUPLE2(bstc_uint_least32_t, BSTC_CTUPLE2(bstc_ratio_c(3600), bstc_ratio_c(1)))
+# if defined(bstc_int_least32_t)
+#  define bstc_chrono_hrs BSTC_CTUPLE2(bstc_int_least32_t, BSTC_CTUPLE2(bstc_ratio_c(3600), bstc_ratio_c(1)))
 # endif
 #endif
 
 
 #ifndef bstc_chrono_days
-# if defined(bstc_uint_least32_t)
-#  define bstc_chrono_days BSTC_CTUPLE2(bstc_uint_least32_t, BSTC_CTUPLE2(bstc_ratio_c(86400), bstc_ratio_c(1)))
+# if defined(bstc_int_least32_t)
+#  define bstc_chrono_days BSTC_CTUPLE2(bstc_int_least32_t, BSTC_CTUPLE2(bstc_ratio_c(86400), bstc_ratio_c(1)))
 # endif
 #endif
 
 
 #ifndef bstc_chrono_weeks
-# if defined(bstc_uint_least32_t)
-#  define bstc_chrono_weeks BSTC_CTUPLE2(bstc_uint_least32_t, BSTC_CTUPLE2(bstc_ratio_c(604800), bstc_ratio_c(1)))
+# if defined(bstc_int_least32_t)
+#  define bstc_chrono_weeks BSTC_CTUPLE2(bstc_int_least32_t, BSTC_CTUPLE2(bstc_ratio_c(604800), bstc_ratio_c(1)))
 # endif
 #endif
 
 
 #ifndef bstc_chrono_months
-# if defined(bstc_uint_least32_t)
-#  define bstc_chrono_months BSTC_CTUPLE2(bstc_uint_least32_t, BSTC_CTUPLE2(bstc_ratio_c(2629746), bstc_ratio_c(1)))
+# if defined(bstc_int_least32_t)
+#  define bstc_chrono_months BSTC_CTUPLE2(bstc_int_least32_t, BSTC_CTUPLE2(bstc_ratio_c(2629746), bstc_ratio_c(1)))
 # endif
 #endif
 
 
 #ifndef bstc_chrono_years
-# if defined(bstc_uint_least32_t)
-#  define bstc_chrono_years BSTC_CTUPLE2(bstc_uint_least32_t, BSTC_CTUPLE2(bstc_ratio_c(31556952), bstc_ratio_c(1)))
+# if defined(bstc_int_least32_t)
+#  define bstc_chrono_years BSTC_CTUPLE2(bstc_int_least32_t, BSTC_CTUPLE2(bstc_ratio_c(31556952), bstc_ratio_c(1)))
 # endif
 #endif
 /// \}
@@ -163,70 +183,70 @@
 /** Convenience types for the duration helpers. */
 /// \{
 #ifndef bstc_chrono_ns_t
-# if bstc_intmax_max >= bstc_uint32_max && defined(bstc_nano)
+# if bstc_intmax_max >= bstc_int32_max && defined(bstc_nano)
 #  define bstc_chrono_ns_t bstc_chrono_dur_t(bstc_chrono_ns)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_us_t
-# if bstc_intmax_max >= bstc_uint32_max && defined(bstc_micro)
+# if bstc_intmax_max >= bstc_int32_max && defined(bstc_micro)
 #  define bstc_chrono_us_t bstc_chrono_dur_t(bstc_chrono_us)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_ms_t
-# if bstc_intmax_max >= bstc_uint16_max && defined(bstc_milli)
+# if bstc_intmax_max >= bstc_int16_max && defined(bstc_milli)
 #  define bstc_chrono_ms_t bstc_chrono_dur_t(bstc_chrono_ms)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_sec_t
-# if defined(bstc_uint_least64_t)
+# if defined(bstc_int_least64_t)
 #  define bstc_chrono_sec_t bstc_chrono_dur_t(bstc_chrono_sec)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_min_t
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_min_t bstc_chrono_dur_t(bstc_chrono_min)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_hrs_t
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_hrs_t bstc_chrono_dur_t(bstc_chrono_hrs)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_days_t
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_days_t bstc_chrono_dur_t(bstc_chrono_days)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_weeks_t
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_weeks_t bstc_chrono_dur_t(bstc_chrono_weeks)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_months_t
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_months_t bstc_chrono_dur_t(bstc_chrono_months)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_years_t
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_years_t bstc_chrono_dur_t(bstc_chrono_years)
 # endif
 #endif
@@ -236,70 +256,70 @@
 /** Convenience function to get the count for the duration helpers. */
 /// \{
 #ifndef bstc_chrono_ns_cnt
-# if bstc_intmax_max >= bstc_uint32_max && defined(bstc_nano)
+# if bstc_intmax_max >= bstc_int32_max && defined(bstc_nano)
 #  define bstc_chrono_ns_cnt(dur) bstc_chrono_dur_cnt(bstc_chrono_ns, dur)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_us_cnt
-# if bstc_intmax_max >= bstc_uint32_max && defined(bstc_micro)
+# if bstc_intmax_max >= bstc_int32_max && defined(bstc_micro)
 #  define bstc_chrono_us_cnt(dur) bstc_chrono_dur_cnt(bstc_chrono_us, dur)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_ms_cnt
-# if bstc_intmax_max >= bstc_uint16_max && defined(bstc_milli)
+# if bstc_intmax_max >= bstc_int16_max && defined(bstc_milli)
 #  define bstc_chrono_ms_cnt(dur) bstc_chrono_dur_cnt(bstc_chrono_ms, dur)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_sec_cnt
-# if defined(bstc_uint_least64_t)
+# if defined(bstc_int_least64_t)
 #  define bstc_chrono_sec_cnt(dur) bstc_chrono_dur_cnt(bstc_chrono_sec, dur)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_min_cnt
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_min_cnt(dur) bstc_chrono_dur_cnt(bstc_chrono_min, dur)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_hrs_cnt
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_hrs_cnt(dur) bstc_chrono_dur_cnt(bstc_chrono_hrs, dur)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_days_cnt
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_days_cnt(dur) bstc_chrono_dur_cnt(bstc_chrono_days, dur)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_weeks_cnt
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_weeks_cnt(dur) bstc_chrono_dur_cnt(bstc_chrono_weeks, dur)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_months_cnt
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_months_cnt(dur) bstc_chrono_dur_cnt(bstc_chrono_months, dur)
 # endif
 #endif
 
 
 #ifndef bstc_chrono_years_cnt
-# if defined(bstc_uint_least32_t)
+# if defined(bstc_int_least32_t)
 #  define bstc_chrono_years_cnt(dur) bstc_chrono_dur_cnt(bstc_chrono_years, dur)
 # endif
 #endif
--- test/boostc/chrono/duration/main.c
+++ test/boostc/chrono/duration/main.c
@@ -17,10 +17,56 @@ int main(int argc, char *argv[])
 
     //--------------------------------------------------------------------------------------------------------
     {
-        bstc_chrono_hrs_t hrs = 1000;
+        bstc_chrono_hrs_t hrs = 100;
+        bstc_chrono_ms_t ms;
     
         printf("bstc_chrono_hrs_cnt(hrs): %" bstc_priimax "\n", (bstc_intmax_t)bstc_chrono_hrs_cnt(hrs));
-        if(bstc_chrono_hrs_cnt(hrs) == 1000)
+        if(bstc_chrono_hrs_cnt(hrs) == 100)
+        {
+            ++num_pass;
+            printf("    passed\n");
+        }
+        else
+        {
+            ++num_fail;
+            printf("    failed\n");
+        }
+    
+        ms = bstc_chrono_dur_cast(bstc_chrono_ms, bstc_chrono_hrs, hrs);
+        printf("bstc_chrono_ms_cnt(ms): %" bstc_priimax "\n", (bstc_intmax_t)bstc_chrono_ms_cnt(ms));
+        if(bstc_chrono_ms_cnt(ms) == 360000000)
+        {
+            ++num_pass;
+            printf("    passed\n");
+        }
+        else
+        {
+            ++num_fail;
+            printf("    failed\n");
+        }
+    }
+
+    //--------------------------------------------------------------------------------------------------------
+    {
+        #define hrsf bstc_chrono_dur_traits(double, bstc_ratio(3600))
+        bstc_chrono_ms_t ms = 100;
+        bstc_chrono_dur_rep(hrsf) hrs;
+    
+        printf("bstc_chrono_ms_cnt(ms): %" bstc_priimax "\n", (bstc_intmax_t)bstc_chrono_ms_cnt(ms));
+        if(bstc_chrono_ms_cnt(ms) == 100)
+        {
+            ++num_pass;
+            printf("    passed\n");
+        }
+        else
+        {
+            ++num_fail;
+            printf("    failed\n");
+        }
+    
+        hrs = bstc_chrono_dur_cast(hrsf, bstc_chrono_ms, ms);
+        printf("bstc_chrono_hrs_cnt(hrs): %f\n", (float)bstc_chrono_dur_cnt(hrsf, hrs));
+        if((0.000028 - 0.000001) <= bstc_chrono_dur_cnt(hrsf, hrs) || bstc_chrono_dur_cnt(hrsf, hrs) <= (0.000028 + 0.000001))
         {
             ++num_pass;
             printf("    passed\n");
