c5ae3b66581a55a64deecd407c4dc911439f674f
Kelleher, Mitchell <mitchell.kelleher@gmail.com>
2025-12-29 14:00:25 -0500

    vscode dev and mac
    Adding some gitignore for vscode development and added the initial work for mac os support in boostc.

---
+++ boostc/config/platform/osx.h
@@ -0,0 +1,13 @@
+//  Copyright (C) 2020-? T. Mitchell Kelleher
+//
+//  Distributed under the Boost Software License, Version 1.0. (See
+//  accompanying file LICENSE_1_0.txt or copy at
+//  http://www.boost.org/LICENSE_1_0.txt)
+#ifndef BOOSTC__CONFIG__PLATFORM__OSX_H
+#define BOOSTC__CONFIG__PLATFORM__OSX_H
+
+#if !defined(BSTC_OSAPI_POSIX) && !defined(BSTC_OSAPI_WINDOWS)
+# define BSTC_OSAPI_POSIX
+#endif
+
+#endif // BOOSTC__CONFIG__PLATFORM__OSX_H
